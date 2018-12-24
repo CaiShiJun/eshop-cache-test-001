676adf4dae8e14f690e0195794b1de47286e28ae
蔡诗珺 <1990908685@qq.com>
2018-12-25 03:05:15 +0800

    增加 56_基于nginx+lua+java完成多级缓存架构的核心业务逻辑（三）

---
+++ 56/eshop-cache/src/main/java/org/github/caishijun/eshop/cache/EshopCacheApplication.java
@@ -0,0 +1,12 @@
+package org.github.caishijun.eshop.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopCacheApplication.class, args);
+    }
+}
