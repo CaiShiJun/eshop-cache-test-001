f6d1cf4a5aeccf884f698b75b9e22854f63e18e9
蔡诗珺 <1990908685@qq.com>
2018-12-27 01:51:45 +0800

    增加 60_缓存数据生产服务中的zk分布式锁解决方案的代码实现（三）

---
+++ 60/eshop-cache/src/test/java/org/github/caishijun/eshop/cache/ZooKeeperTest.java
@@ -0,0 +1,11 @@
+package org.github.caishijun.eshop.cache;
+
+import org.github.caishijun.eshop.cache.zk.ZooKeeperSession;
+
+public class ZooKeeperTest {
+	
+	public static void main(String[] args) {
+		ZooKeeperSession zkSession = ZooKeeperSession.getInstance();
+	}
+	
+}
